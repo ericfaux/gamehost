4b050639281841807804e6eb39d6793ac03c9c2a
ericfaux <67875487+ericfaux@users.noreply.github.com>
2025-12-29 19:29:04 -0500

    Add RefreshCcw icon export

--- components/icons/lucide-react.tsx
+++ components/icons/lucide-react.tsx
@@ -129,6 +129,17 @@ export function StopCircle(props: IconProps) {
   );
 }
 
+export function RefreshCcw(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M3 2v6h6" />
+      <path d="M21 12A9 9 0 0 0 6 5.3L3 8" />
+      <path d="M21 22v-6h-6" />
+      <path d="M3 12a9 9 0 0 0 15 6.7l3-2.7" />
+    </IconBase>
+  );
+}
+
 export function Palette(props: IconProps) {
   return (
     <IconBase {...props}>
