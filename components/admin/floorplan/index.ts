f354d505f9605cf62c7a652b59f27fc2431f2933
ericfaux <67875487+ericfaux@users.noreply.github.com>
2025-12-31 00:17:47 -0500

    Document floor plan components in index.ts
    Added documentation comment for floor plan components.

--- components/admin/floorplan/index.ts
+++ components/admin/floorplan/index.ts
@@ -1,1 +1,15 @@
-export { FloorPlanCard } from "./FloorPlanCard";
+/**
+ * Floor Plan Components
+ *
+ * Components for the admin floor plan UI.
+ */
+
+export { FloorPlanCard } from './FloorPlanCard';
+export { FloorPlanCanvas } from './FloorPlanCanvas';
+export { TableNode, getDefaultLayoutForCapacity, type TableSessionInfo, type TableStatus } from './TableNode';
+export { TableDrawer } from './TableDrawer';
+export { ZoneTabs } from './ZoneTabs';
+export { UnplacedTablesList } from './UnplacedTablesList';
+export { TableInspector } from './TableInspector';
+export { EditModeToolbar } from './EditModeToolbar';
+export { ZoneManagerModal } from './ZoneManagerModal';
